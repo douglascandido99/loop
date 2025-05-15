f0c0872ba3afb84dc1c27632fa262120d3d1e223
Douglas Candido <douglascandido@workmail.com>
2025-05-15 19:17:20 -0300

    chore: fix components folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
-
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Hi!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aut libero ipsum perspiciatis velit, animi, culpa
-        qui accusantium debitis numquam fugit. Voluptatum incidunt consequatur est labore blanditiis quisquam earum
-        sapiente.
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
