61da231d10d975fc3d12cdec406043b2cd658fcb
Douglas Candido <douglascandido@workmail.com>
2025-05-15 18:18:06 -0300

    chore: add heading styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { TimerIcon } from 'lucide-react';
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0);
+import './styles/global.css';
+import './styles/theme.css';
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Heading>
+        Hi!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aut libero ipsum perspiciatis velit, animi, culpa
+        qui accusantium debitis numquam fugit. Voluptatum incidunt consequatur est labore blanditiis quisquam earum
+        sapiente.
+      </p>
     </>
   );
 }
-
-export default App;
